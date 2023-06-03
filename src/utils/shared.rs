ceac34467a54d3c5b71870acc9f67c607cb5f227
pointer-to-bios <pointer-to-bios@outlook.com>
2023-06-03 23:09:38 +0800

    更新共享指针 目前只适用于linux

--- src/utils/shared.rs
+++ src/utils/shared.rs
@@ -1,3 +1,5 @@
+use core::slice;
+
 use nix::{
     errno::Errno,
     fcntl::OFlag,
@@ -20,7 +22,11 @@ pub struct SharedPointer<T> {
 /// 这个结构的`new`和`bind`函数中的参数`size`指申请的字节数，而不是泛型类型的实际大小的数量
 impl<T> SharedPointer<T> {
     pub fn new(name: &str, size: usize) -> Result<Self, Errno> {
-        let fd = sys::mman::shm_open(("/".to_string() + &name.to_string()).as_str(), OFlag::O_RDWR & OFlag::O_CREAT, Mode::S_IRWXU)?;
+        let fd = sys::mman::shm_open(
+            ("/".to_string() + &name.to_string()).as_str(),
+            OFlag::O_RDWR & OFlag::O_CREAT,
+            Mode::S_IRWXU,
+        )?;
         let addr = unsafe {
             sys::mman::mmap(
                 None,
@@ -65,5 +71,29 @@ impl<T> Drop for SharedPointer<T> {
         unsafe { sys::mman::munmap(self.pointer.cast(), self.size).unwrap() };
         unistd::close(self.fd).unwrap();
         sys::mman::shm_unlink(self.name.as_str()).unwrap();
+    }
+}
+
+impl<T> SharedPointer<T> {
+    pub fn slice<'a>(self, addr: usize, mut len: usize) -> &'a [T] {
+        if addr + len > self.size {
+            len = self.size - addr;
+        }
+        unsafe { slice::from_raw_parts((self.pointer as usize + addr) as *mut T, len) }
+    }
+
+    pub fn slice_mut<'a>(self, addr: usize, mut len: usize) -> &'a mut [T] {
+        if addr + len > self.size {
+            len = self.size - addr;
+        }
+        unsafe { slice::from_raw_parts_mut((self.pointer as usize + addr) as *mut T, len) }
+    }
+
+    pub fn at<'a>(self, addr: usize) -> &'a T {
+        unsafe { ((self.pointer as usize + addr) as *mut T).as_ref() }.unwrap()
+    }
+
+    pub fn at_mut<'a>(self, addr: usize) -> &'a mut T {
+        unsafe { ((self.pointer as usize + addr) as *mut T).as_mut() }.unwrap()
     }
 }
