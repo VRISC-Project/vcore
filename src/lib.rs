ce1b6a92818e8000294c58c792c20de02c798d64
pointer-to-bios <pointer-to-bios@outlook.com>
2023-06-12 02:28:10 +0800

    vcore core initialization

--- src/lib.rs
+++ src/lib.rs
@@ -6,21 +6,23 @@ pub mod vrisc;
 use config::Config;
 use memory::Memory;
 use nix::unistd::{self};
+use vrisc::vcore::Vcore;
 
 pub fn run(config: Config) {
     let mut cores = Vec::new();
 
-    for _ in 0..config.cores {
+    for i in 0..config.cores {
         match unsafe { unistd::fork().unwrap() } {
             unistd::ForkResult::Parent { child } => cores.push(child),
             unistd::ForkResult::Child => {
-                vcore(config.memory);
+                vcore(config.memory, i);
                 break;
             }
         }
     }
 }
 
-fn vcore(memory_size: usize) {
+fn vcore(memory_size: usize, id: usize) {
     let memory = Memory::new(memory_size);
+    let core = Vcore::new(id);
 }
--- src/vrisc/vcore.rs
+++ src/vrisc/vcore.rs
@@ -1,1 +1,41 @@
-pub struct Vcore {}
+pub struct Registers {
+    pub x: [u64; 16],
+    pub ip: u64,
+    pub flag: u64,
+    pub ivt: u64,
+    pub kpt: u64,
+    pub upt: u64,
+    pub scp: u64,
+    pub ipdump: u64,
+    pub flagdump: u64,
+}
+
+impl Registers {
+    pub fn new() -> Self {
+        Registers {
+            x: [0; 16],
+            ip: 0,
+            flag: 0,
+            ivt: 0,
+            kpt: 0,
+            upt: 0,
+            scp: 0,
+            ipdump: 0,
+            flagdump: 0,
+        }
+    }
+}
+
+pub struct Vcore {
+    id: usize,
+    regs: Registers,
+}
+
+impl Vcore {
+    pub fn new(id: usize) -> Self {
+        Vcore {
+            id,
+            regs: Registers::new(),
+        }
+    }
+}
