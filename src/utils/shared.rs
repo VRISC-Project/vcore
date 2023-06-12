257a5b881c959f7b9c0ebd39432855133c796565
pointer-to-bios <pointer-to-bios@outlook.com>
2023-06-13 00:03:19 +0800

    为共享指针提供写入功能

--- src/utils/shared.rs
+++ src/utils/shared.rs
@@ -21,7 +21,7 @@ pub struct SharedPointer<T> {
 /// ## 注意
 /// 这个结构的`new`和`bind`函数中的参数`size`指申请的字节数，而不是泛型类型的实际大小的数量
 impl<T> SharedPointer<T> {
-    pub fn new(name: &str, size: usize) -> Result<Self, Errno> {
+    pub fn new(name: String, size: usize) -> Result<Self, Errno> {
         let fd = sys::mman::shm_open(
             ("/".to_string() + &name.to_string()).as_str(),
             OFlag::O_RDWR & OFlag::O_CREAT,
@@ -45,8 +45,8 @@ impl<T> SharedPointer<T> {
         })
     }
 
-    pub fn bind(name: &str, size: usize) -> Result<Self, Errno> {
-        let fd = sys::mman::shm_open(name, OFlag::O_RDWR & OFlag::O_EXCL, Mode::S_IRWXU)?;
+    pub fn bind(name: String, size: usize) -> Result<Self, Errno> {
+        let fd = sys::mman::shm_open(name.as_str(), OFlag::O_RDWR & OFlag::O_EXCL, Mode::S_IRWXU)?;
         let addr = unsafe {
             sys::mman::mmap(
                 None,
@@ -75,25 +75,37 @@ impl<T> Drop for SharedPointer<T> {
 }
 
 impl<T> SharedPointer<T> {
-    pub fn slice<'a>(self, addr: usize, mut len: usize) -> &'a [T] {
+    pub fn slice<'a>(&self, addr: usize, mut len: usize) -> &'a [T] {
         if addr + len > self.size {
             len = self.size - addr;
         }
         unsafe { slice::from_raw_parts((self.pointer as usize + addr) as *mut T, len) }
     }
 
-    pub fn slice_mut<'a>(self, addr: usize, mut len: usize) -> &'a mut [T] {
+    pub fn slice_mut<'a>(&self, addr: usize, mut len: usize) -> &'a mut [T] {
         if addr + len > self.size {
             len = self.size - addr;
         }
         unsafe { slice::from_raw_parts_mut((self.pointer as usize + addr) as *mut T, len) }
     }
 
-    pub fn at<'a>(self, addr: usize) -> &'a T {
+    pub fn at<'a>(&self, addr: usize) -> &'a T {
         unsafe { ((self.pointer as usize + addr) as *mut T).as_ref() }.unwrap()
     }
 
-    pub fn at_mut<'a>(self, addr: usize) -> &'a mut T {
+    pub fn at_mut<'a>(&self, addr: usize) -> &'a mut T {
         unsafe { ((self.pointer as usize + addr) as *mut T).as_mut() }.unwrap()
+    }
+
+    pub fn write(&mut self, addr: usize, t: T) {
+        if addr < self.size {
+            unsafe { *((self.pointer as usize + addr) as *mut T) = t };
+        }
+    }
+
+    pub fn write_slice(&mut self, addr: usize, s: &[T]) {
+        if addr + s.len() < self.size {
+            unsafe { ((self.pointer as usize + addr) as *mut T).copy_from(s.as_ptr(), s.len()) };
+        }
     }
 }
