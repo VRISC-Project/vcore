749fc7923e9f431471ba6b09bf8d6ad68518314b
pointer-to-bios <pointer-to-bios@outlook.com>
2023-07-06 17:27:18 +0800

    更新文档，使用了clap库，没有必要在文档中展示命令用法

--- src/config.rs
+++ src/config.rs
@@ -8,19 +8,19 @@ pub struct Config {
     #[arg(short, long, default_value_t = 1)]
     pub cores: usize,
 
-    ///内存大小
+    /// 内存大小
     #[arg(short, long)]
     pub memory: usize,
 
-    ///固件代码文件
+    /// 固件代码文件
     #[arg(short, long)]
     pub firmware_file: String,
 
-    ///是否开启调试
+    /// 是否开启调试
     #[arg(short, long, default_value_t = false)]
     pub debug: bool,
 
-    ///是否开启外部时钟
+    /// 是否开启外部时钟
     #[arg(short, long, default_value_t = false)]
     pub external_clock: bool,
 }
